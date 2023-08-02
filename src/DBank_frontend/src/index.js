75c67ba2497b4197113b76f5eb33700506adcb74
arjuncvinod <arjuncvinod007@gnail.com>
2023-08-02 14:11:14 +0530

    frontend connected

--- src/DBank_frontend/src/index.js
+++ src/DBank_frontend/src/index.js
@@ -1,19 +1,5 @@
-import { DBank_backend } from "../../declarations/DBank_backend";
+import {dbank} from "../../declarations/DBank_backend"
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await DBank_backend.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
-});
+window.addEventListener("load",function(){
+  console.log("finished loading");
+})
